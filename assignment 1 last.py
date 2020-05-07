b5ac8b7cd889895ff73e14cd69068c6004c08e2f
baldau12 <64821902+baldau12@users.noreply.github.com>
2020-05-07 14:18:58 +0530

    Add files via upload

---
+++ assignment 1 last.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+print("WE,THE PEOPLE OF INDIA,\n\t having solemnly resolved to constitute India into a SOVEREIGNE,! \n\t\t SOCIALIST,SECULAR,DEMOCRATIC REPUBLIC \n\t\t  and to secure to all its citizens")
+
+
+# In[ ]:
+
+
+
+
